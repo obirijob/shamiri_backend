31713bd579facc319ccad69a0cfb29dc7ff7bf89
Job Obiri <obirijob20@gmail.com>
2024-07-08 10:09:02 +0300

    Seed data to db

---
+++ prisma/migrations/20240708070520_remove_created_by_in_journal/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdBy` on the `journals` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `journals` DROP COLUMN `createdBy`;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,37 @@ async function main() {
     data: [{ name: 'Personal' }, { name: 'Work' }, { name: 'Travel' }],
     skipDuplicates: true
   })
+
+  await prisma.journal.createMany({
+    data: [
+      {
+        title: 'First Journal',
+        content: 'This is some content',
+        category: 1,
+        createdAt: new Date(2023, 12, 14, 22, 46, 0, 0)
+      },
+      {
+        title: 'Second Journal',
+        content: 'This is another journal with a very long content',
+        category: 2,
+        createdAt: new Date(2024, 2, 14, 12, 46, 0, 0)
+      },
+      {
+        title: 'Third Journal',
+        content:
+          'This is a journal with a short content. This content needs to be long so that we can visualize how the clipping of the content will work',
+        category: 3,
+        createdAt: new Date(2024, 6, 14, 12, 46, 0, 0)
+      },
+      {
+        title: 'Third Journal',
+        content:
+          'This is a journal with a short content. This content needs to be long so that we can visualize how the clipping of the content will work',
+        category: 2,
+        createdAt: new Date(2024, 6, 12, 12, 46, 0, 0)
+      }
+    ]
+  })
 }
 
 main()
