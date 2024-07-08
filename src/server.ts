b1168b75542655b025f79d0845c98bd0b6d3536f
Job Obiri <obirijob20@gmail.com>
2024-07-08 10:16:00 +0300

    Add essential middlewares

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@
 
 import express from 'express'
 import journalRouter from './routes/journals'
+import authRouter from './routes/auth'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 
 const port = 2000
 
+// middlewares
+app.use(bodyParser.json())
+app.use(cors())
+
 app.use('/journals', journalRouter)
+app.use('/auth', authRouter)
 
 app.listen(port, () => console.log(`App running on port ${port}`))
